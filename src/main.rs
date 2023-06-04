dc9e485041b11a147989572deb6ef4c488a2464e
enchanted-coder <danieltogey@pm.me>
2023-06-05 00:45:58 +0300

    test one

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            println!("Shard {}", ctx.shard_id);             
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = 
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Error creating client");
+
+    if let Err(why) = client.start_shards(2).await {
+        println!("Client error: {:?}", why);
+    }
+}
